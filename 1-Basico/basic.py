430dbdcff6cb6460ce056b17f48cf93c96c2090a
AnaTrzeciak <amgtrzeciak@gmail.com>
2018-11-02 22:43:11 -0300

    criação de arquivos

---
+++ 1-Basico/basic.py
@@ -0,0 +1,18 @@
+#Compilar: python nomedoarquivo.py
+
+print('ALUNO(A)     NOTA')
+print('=======      ====')
+print('ALINE         9.0')
+print('MARIO       10.0')
+print('SERGIO       4.5')
+print('SHIRLEY      7.0')
+
+#numero binario começa com: 0b
+#numero octal começa com: 0o
+#numero hexadecimal começa com: 0x
+
+#decimal para binario: bin(x)
+#decimal para octal: oct(x)
+#decimal para hexadecimal: hex(x)
+
+
---
+++ 1-Basico/variaveis.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+#Variavel nada mais e que um nome que vamos dar a um determinado bloco de memoria. 
+
+banda= 'Banda Calypso'
+musica= 'Cavalo Manco'
+
+print 'A melhor banda do mundo é',banda,'com a musica ',musica 
+
+salario=1800
+
+print'Sou programadora e ganho R$',salario
