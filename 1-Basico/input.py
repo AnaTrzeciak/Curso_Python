618d83bef6cf443c0190e8a623ad11a0c51a641c
AnaTrzeciak <amgtrzeciak@gmail.com>
2018-11-02 23:10:04 -0300

    criação de arquivos

---
+++ 1-Basico/input.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+#sintaxe: variavel=input(string)
+
+idade=input('Qual a sua idade? ')
+nome=input('Qual o seu nome? ')
+
+print("Olá ",nome, " voce tem ",idade, "anos de idade. Bem vindo ao Curso de Python")
+
