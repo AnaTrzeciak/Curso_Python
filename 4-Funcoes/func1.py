b37eadda80ce1655cf29d02e5b1a4f4a2ca24ed9
AnaTrzeciak <amgtrzeciak@gmail.com>
2019-08-19 10:57:56 -0300

    adicao de arquivos

---
+++ 4-Funcoes/func1.py
@@ -0,0 +1,17 @@
+#ecoding:utf-8
+#Como declarar, chamar e usar funcoes.
+#==========================================
+#Sintaxe: 
+#def nome-funcao(argumentos):
+#   código
+#   código
+#   ...
+#   return valor
+#=========================================
+#Argumentos e Retornar valor são opcionais
+
+def mensagem():
+	print("Ola mundo \n")
+
+mensagem()
+
