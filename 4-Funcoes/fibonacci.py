e8813431022180154cd25197ab751959021cbc58
AnaTrzeciak <amgtrzeciak@gmail.com>
2019-09-04 21:00:03 -0300

    adicao de arquivos

--- 4-Funcoes/fibonacci.py
+++ 4-Funcoes/fibonacci.py
@@ -2,7 +2,22 @@
 #Serie de Fibonacci
 #Feito em 02/09/2019 por Ana Maria
 
-def fibonacci():
-  primeiro=0
-  segundo=1
-  return 
+def fibonacci(x):
+ if(x==1):
+   return 0
+ elif(x==2):
+   return 1
+ else: 
+   return fibonacci(x-1) + fibonacci(x-2)
+
+def menu():
+  n=int(input("Exibir ate o termo (maior que 2): "))
+ 
+  for val in range(1,n+1):
+     print(fibonacci(val))
+
+
+while True:
+  menu()
+
+
